885232937fb059b6b11a3405f6c8a4935903ae9b
Avinash Thakur <avinash.thakur2209@gmail.com>
2025-03-14 17:45:24 +0530

    Added navigation for past convo

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -2,8 +2,13 @@ import { Box, Container, Typography } from "@mui/material";
 import newChatIcon from "../../assets/newchat.png"
 import editIcon from "../../assets/edit.png"
 import styles from "./SideNav.module.css"
+import { useNavigate } from "react-router-dom";
 
 function SideNav(){
+    const navigate = useNavigate();
+    function handleClick() {
+        navigate("/history");
+      }
     return(
         <div className={styles.sidenav}>
             <header>
@@ -11,7 +16,7 @@ function SideNav(){
                 <Typography>New Chat</Typography>
                 <img style={{ width : '24px', height: '24px'}} src={editIcon} />
             </header>
-            <div className={styles.pastConvo}>
+            <div className={styles.pastConvo} onClick={handleClick}>
                 <Typography fontWeight={700} fontSize={18}>Past Conversations</Typography>
             </div>
         </div>
